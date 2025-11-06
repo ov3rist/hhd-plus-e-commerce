f2ae153ec3e08fc6941505dbe0c887bc83ad490f
overist <wj@over.ist>
2025-11-06 22:35:57 +0900

    feat: coupon 모듈 초기화

---
+++ src/infrastructure/modules/coupon.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { CouponService } from '@application/coupon.service';
+import {
+  ICouponRepository,
+  IUserCouponRepository,
+} from '@application/interfaces';
+import {
+  CouponRepository,
+  UserCouponRepository,
+} from '@infrastructure/repositories';
+import { CouponController } from '@presentation/coupon';
+
+/**
+ * Coupon Module
+ * 쿠폰 관리 모듈
+ */
+@Module({
+  controllers: [CouponController],
+  providers: [
+    {
+      provide: ICouponRepository,
+      useClass: CouponRepository,
+    },
+    {
+      provide: IUserCouponRepository,
+      useClass: UserCouponRepository,
+    },
+    CouponService,
+  ],
+  exports: [CouponService],
+})
+export class CouponModule {}
