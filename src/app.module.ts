dbd241f71302086998d15421e16eb1d0448ae608
overist <wj@over.ist>
2025-11-07 12:33:40 +0900

    refactor: appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {
   ProductModule,
   UserModule,
@@ -19,7 +17,7 @@ import {
     CouponModule,
     SchedulerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
